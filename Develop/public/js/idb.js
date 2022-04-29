105e5ca8acd6b472de13b775a8f24738712efa4b
REX <cssmith431@yahoo.com>
2022-04-29 13:10:54 -0400

    saved offline

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -6,10 +6,16 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('new_transaction', {autoIncrement: true});
 
+   
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
     if(navigator.online) {
 
     }
-};
+}
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
