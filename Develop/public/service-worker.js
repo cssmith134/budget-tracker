9192d1caa571b8fbdc63f4fd9bbe886f2c9a033d
REX <cssmith431@yahoo.com>
2022-04-29 15:27:21 -0400

    fixed bug

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,22 +1,21 @@
 const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01',
+const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/styles.css",
-    "./js/idb.js",
-    "./js/index.js",
-    "./manifest.json",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png",
+    "/index.html",
+    "/css/styles.css",
+    "/js/idb.js",
+    "/js/index.js",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png",
 ];
 
 
@@ -24,8 +23,10 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-          console.log('installing cache : ' + CACHE_NAME)
+          console.log('installing cache : ' + CACHE_NAME, cache)
           return cache.addAll(FILES_TO_CACHE)
+        }).catch(err => {
+            console.log(err)
         })
       );
 });
@@ -54,7 +55,7 @@ self.addEventListener("activate", function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
       caches.match(e.request).then(function (request) {
-        if (request) {console.log('responding with cache : ' + e.request.url)
+        if (request) {console.log('CACHE_NAME : ' + e.request.url)
           return request
         } else {console.log('file is not cached, fetching : ' + e.request.url)
           return fetch(e.request)
